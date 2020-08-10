070f44d446a2aff85a9f57f9cea35c6f40548df6
lifangjin <Lfj360732@>
2020-08-10 23:28:51 +0800

    Initial commit

---
+++ src/main/java/com/lfj/example/singlepattern/SinglePatternApplication.java
@@ -0,0 +1,13 @@
+package com.lfj.example.singlepattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglePatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinglePatternApplication.class, args);
+    }
+
+}
